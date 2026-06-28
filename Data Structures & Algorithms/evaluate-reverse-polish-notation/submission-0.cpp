35e97b6da4e358d14a7093f76c5a384a6ddaa86d
Harshita Jain <jain07.harshita@gmail.com>
2026-06-29 00:43:05 +0530

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+       //1,2.. 1+2.. 3,3...3*3...9,4...9-4
+
+       //basically push numbers into stack until u reach operand...then pop first two numbers apply operand push result in stack and repeat 
+    
+        stack<int> st;
+
+        for(string& token : tokens){
+            if(token == "+" || token == "-" || token == "*" || token == "/"){
+                
+                int b = st.top(); st.pop();  
+                int a = st.top(); st.pop();  
+
+                if(token == "+") st.push(a + b);
+                else if(token == "-") st.push(a - b);
+                else if(token == "*") st.push(a * b);
+                else st.push(a / b);
+            }
+            else{
+                st.push(stoi(token)); //stoi means string to integer
+            }
+        }
+        return st.top();
+}};
